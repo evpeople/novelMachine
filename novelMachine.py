8cd2fbadfaaf8fb4a660ce575a0feaf5ab5410d8
hangcaihui <wangzhe177@bupt.edu.cn>
2020-07-16 21:59:19 +0800

    增加了对404的判断

--- novelMachine.py
+++ novelMachine.py
@@ -11,14 +11,20 @@ from email.utils import formataddr
 import os
 from apscheduler.schedulers.blocking import BlockingScheduler
 
+
 def getOnePage(url):
 
     # 下面的url是待抓取的小说页面，也就是传入的参数
     #url = "http://www.biqu6.com/23_23554/27344021.html"
     response = requests.get(url)
+
+    while response.status_code == 404:
+        time.sleep(120)
+        response = requests.get(url)
+
     bianma = response.encoding
     wenzhang = response.text
-    wenzhang=wenzhang.encode(bianma)
+    wenzhang = wenzhang.encode(bianma)
     wenzhang = wenzhang.decode("utf-8")
     #上面在抓取并解码，下面是形成一个满足beautifulsoup的变量
     cheng = BeautifulSoup(wenzhang, 'html.parser')
@@ -39,20 +45,21 @@ def getOnePage(url):
     title = re.sub("正文卷", "", title)
     #洗标题
 
-    hf=open("temp.txt","w+")
+    hf = open("temp.txt", "w+")
     hf.write(guo)
     mySender = ""
-    myPass= ""
-    myUser= ""
+    myPass = ""
+    myUser = ""
 
-    msg=MIMEText(title+"\n\n"+guo,'plain','utf-8')
-    msg['From']=formataddr(["小说机",mySender])  # 括号里的对应发件人邮箱昵称、发件人邮箱账号
-    msg['To']=formataddr(["FK",myUser])              # 括号里的对应收件人邮箱昵称、收件人邮箱账号
-    msg['Subject']=title
-    server=smtplib.SMTP_SSL("smtp.exmail.qq.com", 465)
+    msg = MIMEText(title+"\n\n"+guo, 'plain', 'utf-8')
+    msg['From'] = formataddr(["小说机", mySender])  # 括号里的对应发件人邮箱昵称、发件人邮箱账号
+    msg['To'] = formataddr(["FK", myUser])              # 括号里的对应收件人邮箱昵称、收件人邮箱账号
+    msg['Subject'] = title
+    server = smtplib.SMTP_SSL("smtp.exmail.qq.com", 465)
     server.login(mySender, myPass)
-    server.sendmail(mySender,[myUser,],msg.as_string())
+    server.sendmail(mySender, [myUser, ], msg.as_string())
     server.quit()
+
 
 def getCatlogy(url):
     #下面为带传入的参数
@@ -63,21 +70,20 @@ def getCatlogy(url):
     url3 = "http://www.biqu6.com/25_25220/"
     url4 = "http://www.biqu6.com/48_48213/"
 
-    if url1==url:
-        d="kbw.txt"
-    elif url2==url:
-        d="wsx.txt"
-    elif url3==url:
-        d="kbf.txt"
-    elif url4==url:
-        d="ksf.txt"    
+    if url1 == url:
+        d = "kbw.txt"
+    elif url2 == url:
+        d = "wsx.txt"
+    elif url3 == url:
+        d = "kbf.txt"
+    elif url4 == url:
+        d = "ksf.txt"
     response = requests.get(url)
     bianma = response.encoding
     CatLogy = response.text
     CatLogy = CatLogy.encode(bianma)
     CatLogy = CatLogy.decode("utf-8")
 
-    
     #需要if判断，来增加小说，可重构
     with open(d, "r") as f:
         i = 1
@@ -101,35 +107,38 @@ def getCatlogy(url):
     if line == linex:
         return 0
     else:
-        p=r"<meta property="
-        q=r'"og:novel:latest_chapter_url"'
-        linex = re.sub(p,"",linex)
-        linex = re.sub(q,"",linex)
-        linex = re.sub("content=","",linex)
-        linex = re.sub("/>","",linex)
-        linex = re.sub('"',"",linex)
+        p = r"<meta property="
+        q = r'"og:novel:latest_chapter_url"'
+        linex = re.sub(p, "", linex)
+        linex = re.sub(q, "", linex)
+        linex = re.sub("content=", "", linex)
+        linex = re.sub("/>", "", linex)
+        linex = re.sub('"', "", linex)
         #更改了则进行抓取
         return linex
 
+
 def tick():
-    web = {'kbw':'http://www.biqu6.com/23_23554/','wsx':'http://www.biqu6.com/49_49868/','kbf':'http://www.biqu6.com/25_25220/','ksf':'http://www.biqu6.com/48_48213/'}
-    fil=open("logg.txt","a+")
+    web = {'kbw': 'http://www.biqu6.com/23_23554/', 'wsx': 'http://www.biqu6.com/49_49868/',
+           'kbf': 'http://www.biqu6.com/25_25220/', 'ksf': 'http://www.biqu6.com/48_48213/'}
+    fil = open("logg.txt", "a+")
     for x in web.values():
         time.sleep(10)
-        h=getCatlogy(x)
-        if h==0:
-            
-            localtime = time.asctime( time.localtime(time.time()) )
+        h = getCatlogy(x)
+        if h == 0:
+
+            localtime = time.asctime(time.localtime(time.time()))
             str(localtime)
             fil.write("没更新  "+localtime+"\n")
             continue
         else:
             time.sleep(120)
             getOnePage(h)
+
     fil.write("\n\n")
     fil.close()
 
 
 sched = BlockingScheduler()
-sched.add_job(tick,'interval', seconds=900,max_instances=100)
+sched.add_job(tick, 'interval', seconds=900, max_instances=100)
 sched.start()
